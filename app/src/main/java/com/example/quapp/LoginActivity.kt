2c5fbe5f69ad9b06d530cb286cf0c4ad35cb2a56
LoukasPap <loukas.pap@hotmail.gr>
2021-11-23 17:04:52 +0200

    Create navigation between activity_login.xml and activity_register.xml.

--- app/src/main/java/com/example/quapp/LoginActivity.kt
+++ app/src/main/java/com/example/quapp/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.quapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val fab: View = findViewById(R.id.goto_reg_fab)
+
+        fab.setOnClickListener{
+            val register_screen = Intent(this, RegisterActivity::class.java)
+            startActivity(register_screen)
+        }
+
     }
 }
--- app/src/main/java/com/example/quapp/RegisterActivity.kt
+++ app/src/main/java/com/example/quapp/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.example.quapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val fab: View = findViewById(R.id.goto_log_fab)
+
+        fab.setOnClickListener{
+            val login_screen = Intent(this, LoginActivity::class.java)
+            startActivity(login_screen)
+        }
     }
 }
