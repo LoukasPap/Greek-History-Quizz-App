2447048b5b769a5f9dd0c5959ebf72dc7def1554
LoukasPap <loukas.pap@hotmail.gr>
2021-12-27 19:17:56 +0200

    Update User Model

--- app/src/main/java/com/example/quapp/User.kt
+++ app/src/main/java/com/example/quapp/User.kt
@@ -4,11 +4,13 @@ import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
 data class User(
-    var username: String? = null,
+    var username: String? = "",
     var email: String? = null,
     var password: String? = null,
     @field:JvmField
     val isGreek: Boolean? = null,
     @ServerTimestamp
-    val timestamp: Date? = null
+    val timestamp: Date? = null,
+    var level: Int? = null,
+    var avatar: Int? = 0,
 )
