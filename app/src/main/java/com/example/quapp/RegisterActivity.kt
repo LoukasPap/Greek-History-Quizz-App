60c32b2943862f7c5132afaa2b1408431dd9d308
LoukasPap <loukas.pap@hotmail.gr>
2022-01-01 18:06:37 +0200

    Add email and password input checks before registration

--- app/src/main/java/com/example/quapp/RegisterActivity.kt
+++ app/src/main/java/com/example/quapp/RegisterActivity.kt
@@ -9,6 +9,7 @@ import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -17,29 +18,77 @@ import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var email: TextInputEditText
-    private lateinit var password : TextInputEditText
-    private lateinit var isGreek : CheckBox
-    private lateinit var registerButton: Button
-    private lateinit var fab: View
+    private lateinit var emailInput: TextInputEditText
+    private lateinit var emailField : TextInputLayout
+    private lateinit var passwordInput : TextInputEditText
+    private lateinit var passwordField : TextInputLayout
+    private lateinit var registerBtn: Button
+    private lateinit var gotoLoginBtn: View
 
     private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        initUIComponents()
+        initUI()
 
         auth = Firebase.auth
-        registerButton.setOnClickListener {
-            registerUser(stringify(email), stringify(password))
+
+        registerBtn.setOnClickListener {
+            if (!checkFields()) {
+                registerUser(stringify(emailInput), stringify(passwordInput))
+            }
         }
 
-        fab.setOnClickListener{
+        gotoLoginBtn.setOnClickListener{
             gotoLoginForm()
         }
-
     }
+
+    private fun checkFields(): Boolean {
+        var hasError = false
+        var email = stringify(emailInput)
+        var emailErrorText = ""
+        var emailErrorCounter = 1
+
+        var password = stringify(passwordInput)
+        var passwordErrorText = ""
+        var passwordErrorCounter = 1
+
+        if (email.isEmpty()) {
+            emailErrorText += "${emailErrorCounter++}. Email address can not be empty\n"
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailErrorText += "${emailErrorCounter++}. Invalid email address\n"
+        }
+
+        val specialCharactersPattern = Regex("\\W")
+        if (password.isEmpty()) {
+            passwordErrorText += "${passwordErrorCounter++}. Password must not be empty\n"
+        }
+        if (specialCharactersPattern.containsMatchIn(password)) {
+            passwordErrorText += "${passwordErrorCounter++}. Password must only consist of letters, numbers and underscores\n"
+        }
+        if (5 > password.length || password.length > 20) {
+            passwordErrorText += "${passwordErrorCounter++}. Password must be 5 to 20 characters long"
+        }
+
+        if (emailErrorText.isNotEmpty()) {
+            emailField.error = emailErrorText
+            hasError = true
+        } else {
+            emailField.error = null
+        }
+        if (passwordErrorText.isNotEmpty()) {
+            passwordField.error = passwordErrorText
+            hasError = true
+        } else {
+            passwordField.error = null
+        }
+
+        return hasError
+    }
+
     private fun registerUser(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email,password)
             .addOnCompleteListener(this) { task ->
@@ -55,22 +104,21 @@ class RegisterActivity : AppCompatActivity() {
 
                 } else {
                     Log.w("INS", "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
-
+                    Toast.makeText(baseContext, "Registration failed.", Toast.LENGTH_SHORT).show()
                 }
             }
     }
 
-    private fun initUIComponents() {
-        email = findViewById(R.id.email_field)
-        password = findViewById(R.id.passwordLoginField)
-        isGreek = findViewById(R.id.isGreek_field);
-        registerButton = findViewById(R.id.register_button)
-        fab = findViewById(R.id.goto_log_fab)
+    private fun initUI() {
+        emailInput = findViewById(R.id.email_input)
+        emailField = findViewById(R.id.email_field)
+        passwordInput = findViewById(R.id.register_input)
+        passwordField = findViewById(R.id.register_field)
+        registerBtn = findViewById(R.id.register_button)
+        gotoLoginBtn = findViewById(R.id.goto_log_fab)
     }
 
     private fun writeUserToFirestore(userInfo: User, docId: String) {
-        Log.d("HERE", "FIRESTORE ")
         val db = Firebase.firestore
         db.collection("users").document(docId).set(userInfo)
     }
