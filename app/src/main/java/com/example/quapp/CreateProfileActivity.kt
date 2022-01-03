b8f551d14f5fbdf3bcd41e25c1d9267f3427b7c6
LoukasPap <loukas.pap@hotmail.gr>
2022-01-03 18:08:09 +0200

    Add checks to username field in create profile activity

--- app/src/main/java/com/example/quapp/CreateProfileActivity.kt
+++ app/src/main/java/com/example/quapp/CreateProfileActivity.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quapp.menu_dashboard.UserMenu
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -17,6 +18,7 @@ import com.google.firebase.ktx.Firebase
 class CreateProfileActivity : AppCompatActivity() {
 
     private lateinit var finishButton: Button
+    private lateinit var usernameField: TextInputLayout
     private lateinit var usernameInput: TextInputEditText
 
     private lateinit var recyclerView: RecyclerView
@@ -39,32 +41,54 @@ class CreateProfileActivity : AppCompatActivity() {
         recyclerView.layoutManager = layoutManager
         var adapter = AvatarRecyclerAdapter()
         recyclerView.adapter = adapter
-        adapter.setOnItemClickListener(object : AvatarRecyclerAdapter.onItemClickListener {
+        adapter.setOnItemClickListener(object : AvatarRecyclerAdapter.OnItemClickListener {
             override fun onItemClick(position: Int) {
                 Toast.makeText(this@CreateProfileActivity, "Selected item no.$position", Toast.LENGTH_SHORT).show()
+
             }
         })
 
         finishButton.setOnClickListener {
-            completeRegistration(userId, usernameInput.text.toString(), 1, 0)
-            Toast.makeText(this, "Completed", Toast.LENGTH_LONG).show()
-            val userScreen = Intent(this, UserMenu::class.java)
-            startActivity(userScreen)
+            if (checkFields()) {
+                completeRegistration(userId, usernameInput.text.toString(),0)
+                Toast.makeText(this, "Completed", Toast.LENGTH_LONG).show()
+                val userScreen = Intent(this, UserMenu::class.java)
+                startActivity(userScreen)
+            }
         }
     }
 
-    private fun completeRegistration(uid: String?, username: String, avatar: Int, level: Int) {
+    private fun checkFields(): Boolean {
+        var username = usernameInput.text.toString()
+        var usernameErrorCounter = 1
+        var usernameErrorText = ""
+
+        val specialCharactersPattern = Regex("\\W")
+        if (specialCharactersPattern.containsMatchIn(username)) {
+            usernameErrorText += "${usernameErrorCounter++}. Username can only consist of letters, numbers and underscores\n"
+        }
+        if (username.length < 3) {
+            usernameErrorText += "${usernameErrorCounter++}. Username should be at least 3 characters"
+        }
+
+        if (usernameErrorText.isNotEmpty()){
+            usernameField.error = usernameErrorText
+        }
+        return usernameErrorText.isEmpty()
+    }
+
+    private fun completeRegistration(uid: String?, username: String, level: Int) {
         val db = Firebase.firestore
         db.collection("users").document(uid!!)
             .update(mapOf(
                 "username" to username,
-                "avatar" to avatar,
                 "level" to level,
             ))
     }
 
     private fun initUI() {
         finishButton = findViewById(R.id.finishButton)
+        usernameField = findViewById(R.id.usernameField)
         usernameInput = findViewById(R.id.usernameInput)
         recyclerView = findViewById(R.id.avatar_recycler_view)
     }
